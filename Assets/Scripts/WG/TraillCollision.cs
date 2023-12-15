6efe641845717a8abdb90c80ffbe4fc613984f44
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2023-12-16 02:18:37 +0900

    feat : traill collision

---
+++ Assets/Scripts/WG/TraillCollision.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TrailRenderer))]
+public class TraillCollision : MonoBehaviour
+{
+    TrailRenderer myTrail;
+
+    //트레일 렌더러 특성상 콜라이더를 다른오브젝트에 붙이고 그걸 가져오기
+    [SerializeField] GameObject coliderGameObject;
+
+    EdgeCollider2D myCollider;
+
+    void Awake()
+    {
+        myTrail = GetComponent<TrailRenderer>();
+        coliderGameObject.AddComponent<EdgeCollider2D>();
+        myCollider = coliderGameObject.GetComponent<EdgeCollider2D>();
+
+        myCollider.edgeRadius = myTrail.startWidth / 2;
+    }
+
+    void Update()
+    {
+        SetColliderPointsFromTrail(myTrail, myCollider);
+    }
+
+    void SetColliderPointsFromTrail(TrailRenderer trail, EdgeCollider2D edge)
+    {
+        //포인트 담을 리스트들
+        List<Vector2> points = new List<Vector2>();
+
+        for (int position = 0; position < trail.positionCount; position++)
+        {
+            points.Add(trail.GetPosition(position));
+        }
+        edge.SetPoints(points);
+    }
+}
