a1e99151bb078e1e45346ffc61b5fd171e444718
DDuAAtGhui <135325796+DDuAAtGhui@users.noreply.github.com>
2023-12-17 08:16:09 +0900

    fix : fadeout

--- Assets/Scripts/WG/UIController.cs
+++ Assets/Scripts/WG/UIController.cs
@@ -1,14 +1,34 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textMeshProUGUI;
-
+    [SerializeField] GameObject fadeoutPanel;
     private void Update()
     {
         textMeshProUGUI.text = GameObject.FindWithTag("Player").transform.position.y.ToString("F1") + "M";
+
+        if (GameManager.instance.isDead)
+        {
+            fadeoutPanel.GetComponent<Image>().enabled = true;
+            fadeoutPanel.GetComponent<DOTweenAnimation>().DOPlay();
+        }
+
+        else
+        {
+            fadeoutPanel.GetComponent<Image>().enabled = false;
+            fadeoutPanel.GetComponent<DOTweenAnimation>().DOPause();
+        }
+    }
+
+    public void SceneLoad(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
